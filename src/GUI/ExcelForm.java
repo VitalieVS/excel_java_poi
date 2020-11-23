e19562b02ae3f1dec31e6b807e859f4462a9d4f0
VitalieVSV <dut66AHJCT>
2020-11-23 14:44:10 +0200

    Added CreateTable.Java implementing CellArray , also updated CellArray

--- src/GUI/ExcelForm.java
+++ src/GUI/ExcelForm.java
@@ -20,18 +20,6 @@ public class ExcelForm implements CellArray {
     private JFileChooser fc;
     private File file;
 
-    static String[] columnNames = {"Nume", "Prenume", "Grupa", "Buget"};
-
-    Object[][] data = {
-            {"Creanga", "Ion", "IS21Z", "Da"},
-            {"Turcanu", "Tudor", "IS21Z", "Da"},
-            {"Lungu", "Andrei", "IS21Z", "Nu"},
-            {"Bazaochi", "Dumitru", "IS21Z", "Da"},
-            {"Popov", "Marinela", "IS21Z", "Da"},
-            {"Popov", "Dumitru", "IS21Z", "Da"},
-            {"Eminescu", "MIhai", "IS21Z", "Nu"}
-    };
-
     public ExcelForm() {
         OpenFile.addActionListener(new ActionListener() {
             @Override
@@ -70,34 +58,6 @@ public class ExcelForm implements CellArray {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        InsertRows();
-    }
-
-    public void InsertRows() {
-        JFrame frame = new JFrame("Inserting rows in the table!");
-        JPanel panel = new JPanel();
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
-        JTable table = new JTable(model);
-        JScrollPane scrollPane = new JScrollPane(table);
-        table.setFillsViewportHeight(true);
-        panel.setLayout(new BorderLayout());
-        panel.add(table.getTableHeader(), BorderLayout.PAGE_START);
-        panel.add(table, BorderLayout.CENTER);
-        //Insert first position
-       // model.insertRow(0, new Object[]{"Ranjan", "50"});
-        //Insert 4 position
-       // model.insertRow(3, new Object[]{"Amar", "600"});
-        //Insert last position
-       // model.insertRow(table.getRowCount(),new Object[]{"Sushil","600"});
-        //setReorderingAllowed(boolean reorderingAllowed)
-        table.setSelectionModel(new ForcedListSelectionModel());
-        table.getTableHeader().setReorderingAllowed(false);
-        panel.add(table);
-        frame.add(panel);
-        frame.setSize(300,300);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
     }
 
     public static void main(String[] args) {
--- src/data/CellArray.java
+++ src/data/CellArray.java
@@ -4,6 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface CellArray {
+    static String[] columnNames = {"Nume", "Prenume", "Grupa", "Buget"};
+
+    Object[][] data = {
+            {"Creanga", "Ion", "IS21Z", "Da"},
+            {"Turcanu", "Tudor", "IS21Z", "Da"},
+            {"Lungu", "Andrei", "IS21Z", "Nu"},
+            {"Bazaochi", "Dumitru", "IS21Z", "Da"},
+            {"Popov", "Marinela", "IS21Z", "Da"},
+            {"Popov", "Dumitru", "IS21Z", "Da"},
+            {"Eminescu", "MIhai", "IS21Z", "Nu"}
+    };
+
     List<CellStringValueData> stringValueList = new ArrayList<>();
     List<CellNumericValueData> numericValueList = new ArrayList<>();
 }
