59693d567abc6da2e1ef30034ba99fecf25bcc48
VitalieVSV <dut66AHJCT>
2020-11-25 18:51:43 +0200

    Removed unnecessary data

--- src/models/CellArrayModelInterface.java
+++ src/models/CellArrayModelInterface.java
@@ -6,16 +6,6 @@ import java.util.List;
 public interface CellArrayModelInterface {
    String[] columnNames = {"Nume", "Prenume", "Grupa", "Buget"};
 
-    Object[][] data = {
-            {"Creanga", "Ion", "IS21Z", "Da"},
-            {"Turcanu", "Tudor", "IS21Z", "Da"},
-            {"Lungu", "Andrei", "IS21Z", "Nu"},
-            {"Bazaochi", "Dumitru", "IS21Z", "Da"},
-            {"Popov", "Marinela", "IS21Z", "Da"},
-            {"Popov", "Dumitru", "IS21Z", "Da"},
-            {"Eminescu", "MIhai", "IS21Z", "Nu"}
-    };
-
     List<CellStringValueModel> stringValueList = new ArrayList<>();
     List<CellNumericValueModel> numericValueList = new ArrayList<>();
     List<TXTFileModel> textFileList = new ArrayList<>();
